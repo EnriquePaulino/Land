68cbef8bd9145dd688e91c9c7baba5c90c9370e8
EnriquePaulino <paulinoenrique@gmail.com>
2019-03-26 16:20:38 -0400

    Login View Model

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,19 @@
 
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
